ad6062b66839cbfb7f89e6b9ac2133268c05a877
silver_daniel97 <silver.daniel97@gmail.com>
2017-04-12 14:42:38 -0300

    changed version

--- MenuSlider/SideMenuFooterViewController.swift
+++ MenuSlider/SideMenuFooterViewController.swift
@@ -12,11 +12,11 @@ class SideMenuFooterViewController: UIViewController {
 
 	@IBOutlet var footerView: UIView!
 	@IBOutlet weak var label: UILabel!
-	
+
 	public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
 		super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
 	}
-	
+
 	required init?(coder aDecoder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
 	}
